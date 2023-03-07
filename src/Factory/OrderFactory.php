d30103f0033ea0ddf3a50c59019e644495e550d3
khanhchi <le.khanhchi@yahoo.fr>
2023-03-07 17:25:04 +0100

    create OrderFactory with function create & createItem

---
+++ src/Factory/OrderFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use App\Entity\Order;
+use App\Entity\Product;
+use App\Entity\OrderItem;
+
+class OrderFactory {
+    
+    /**
+     * Creates an order.
+     *
+     * @return Order
+     */
+    public function create (User $user) :Order
+    {
+        $order = new Order();
+        $order
+            ->setUser($user)
+            ->setStatus(Order::STATUS_CART)
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable());
+
+        return $order;
+    }
+
+      /**
+     * Creates an item for a product.
+     *
+     * @param Product $product
+     *
+     * @return OrderItem
+     */
+    public function createItem(Product $product): OrderItem 
+    {
+        $item = new OrderItem();
+        $item->setProduct($product);
+        $item->setQuantity(1);
+        return $item;
+    }
+}
